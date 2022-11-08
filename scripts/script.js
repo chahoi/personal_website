4cdad39c5503eb6fad784523ae3bd6b127a3e1b0
Jake Choi <9822cj1@gmail.com>
2022-11-08 12:02:10 -0800

    fix objects

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-course1 = {
+let course1 = {
     code: "ACIT 1620" , 
     name: "Web Fundamental Technologies",
 }
-course2 = {
+let course2 = {
     code: "ACIT 1640" , 
     name: "Introduction to Systems Administration",
 }
-course3 = {
+let course3 = {
     code: "ACIT 1630" , 
     name: "Database Systems",
 }
