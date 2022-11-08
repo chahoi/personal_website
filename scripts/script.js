394d20b1716930dc9440dd1c9d471cabe938f455
Jake Choi <9822cj1@gmail.com>
2022-11-08 11:57:21 -0800

    create objects course 1,2,3 and create constant array courseLists

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+course1 = {
+    code: "ACIT 1620" , 
+    name: "Web Fundamental Technologies",
+}
+course2 = {
+    code: "ACIT 1640" , 
+    name: "Introduction to Systems Administration",
+}
+course3 = {
+    code: "ACIT 1630" , 
+    name: "Database Systems",
+}
+
+const courseLists = [course1,course2,course3]
